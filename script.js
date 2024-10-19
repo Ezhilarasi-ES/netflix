2b250085d6332901a9fabf3db71eb771e7867852
Ezhilarasi-ES <ezhiles2002@gmail.com>
2024-10-19 18:04:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+        $(document).ready(function(){
+            $("#p1").click(function(){
+                $("#p2").fadeToggle(500)
+            })
+        })
+
+        $(document).ready(function(){
+            $("#que2").click(function(){
+                $("#ans2").fadeToggle(500)
+            })
+        })
+
+        $(document).ready(function(){
+            $("#que3").click(function(){
+                $("#ans3").fadeToggle(500)
+            })
+        })
+
+        $(document).ready(function(){
+            $("#que4").click(function(){
+                $("#ans4").fadeToggle(500)
+            })
+        })
+
+        $(document).ready(function(){
+            $("#que5").click(function(){
+                $("#ans5").fadeToggle(500)
+            })
+        })
+
+        $(document).ready(function(){
+            $("#que6").click(function(){
+                $("#ans6").fadeToggle(500)
+            })
+        })
+    
